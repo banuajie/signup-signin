c78da7f35e64014039427fcbbdfc44e4849d529c
Nugraha Banuajie <nugrahabanuajie@gmail.com>
2023-02-15 15:47:32 +0700

    hidden button Logout at Sign Up Page and Sign In Page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router";
 
 const Header = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
     return (
         <>
             <section id="header" className="sticky-top">
                 <nav className="navbar bg-success pt-3 pb-3">
                     <div className="container">
                         <p className="navbar-brand my-auto fs-3 text-white">Sign In and Sign Up Aplication</p>
-                        <form className="d-flex">
-                            <button className="btn btn-danger btn-sm" type="submit">
+                        <form
+                            className="d-flex"
+                            onSubmit={(event) => {
+                                event.preventDefault();
+
+                                // navigate to sign in page after click button "Logout"
+                                navigate("/sign-in-page");
+                            }}
+                        >
+                            <button className="btn btn-danger btn-sm" type="submit" hidden={location.pathname === "/" || location.pathname === "/sign-in-page"}>
                                 Logout
                             </button>
                         </form>
